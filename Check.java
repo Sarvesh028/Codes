4b552a7fbf462d33483daa5c7c26c42f5fd10e25
Sarvesh Kumar <sk028431@gmail.com>
2023-06-14 00:03:28 +0530

    new file

---
+++ Check.java
@@ -0,0 +1,19 @@
+// looops using taking input from user:
+
+import java.util.Scanner;
+
+public class Check {
+    
+    public static void main(String[] args) {
+        
+        Scanner Sc = new Scanner(System.in);
+        System.out.println("Enter the Number");
+        int n = Sc.nextInt();
+
+        for (int i = 0 ; i <= n; i++  ){
+        
+            System.out.println(i);
+        }
+
+    }
+}
