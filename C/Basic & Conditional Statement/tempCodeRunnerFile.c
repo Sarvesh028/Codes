63c373329472a1dab7556063f9f229a0a5fa27a5
Sarvesh Kumar <sk028431@gmail.com>
2023-10-11 09:39:03 +0530

    if else

--- C/Basic & Conditional Statement/tempCodeRunnerFile.c
+++
@@ -1,9 +0,0 @@
-  // if (x > y)
-    // {
-    //     printf("\n %d is greater than %d" y);
-    // }
-    // else
-    // {
-    //     printf("\n %d is greater than " x);
-    // }
-    // retrun 0;
